ab976c2fa169af23a9d7d1de061a52a8e79ffe82
BloodTaint <Bloodtaint@yahoo.com>
2023-04-03 23:57:10 +0300

    edit templates

--- culinary_recipes/staticfiles/style/app.js
+++ culinary_recipes/staticfiles/style/app.js
@@ -4,14 +4,13 @@ function onLoad() {
     const btnConnectToUs = document.getElementsByClassName('hiddenContactFormBtn')[0]
     const formWrapper = document.getElementsByClassName('form-wrapper')[0]
     const connect = document.getElementsByClassName('contactFormWrapper')[0]
-    connect.id = 'hide'
-    const foot = document.getElementsByClassName('footer')[0]
+    if (connect) {
+        connect.id = 'hide'
 
-    const emptyDiv = document.createElement('div')
-    connect.replaceChildren(emptyDiv)
-
-
-    btnConnectToUs.addEventListener('click', connectToUs)
+        const emptyDiv = document.createElement('div')
+        connect.replaceChildren(emptyDiv)
+        btnConnectToUs.addEventListener('click', connectToUs)
+    }
 
 
     function connectToUs() {
@@ -19,16 +18,42 @@ function onLoad() {
         if (connect.id === 'hide') {
             connect.style.display = 'grid'
             connect.id = 'show'
-            // foot.style.backgroundColor = '#e68b5e'
 
         } else {
             connect.style.display = 'none'
             connect.id = 'hide'
-            // foot.style.backgroundColor = '#ffffff'
-
         }
+    }
+
+// modal
+
+    let modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    let btn = document.getElementById("myBtn");
+    btn.addEventListener('click', openModal)
 
 
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    function openModal() {
+        modal.style.display = "block";
+        console.log('test')
+    }
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
     }
 }
 

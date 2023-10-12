f78135e5ba3468e71afa460423d2deeb261211af
Andon-ov <bloodtaint@yahoo.com>
2023-10-12 08:09:05 +0300

    change docker command to start postgre (add database name), add is_active in managers.py so superuser can login normally

--- culinary_recipes/culinary_recipes/auth_app/managers.py
+++ culinary_recipes/culinary_recipes/auth_app/managers.py
@@ -4,23 +4,23 @@ from django.contrib.auth.hashers import make_password
 
 class AppUserManager(base_user.BaseUserManager):
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, is_active=True, **extra_fields):
 
         if not email:
             raise ValueError("The given email must be set")
         email = self.normalize_email(email)
 
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, is_active=is_active, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, is_active=True, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, is_active, **extra_fields)
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, is_active=True, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -29,4 +29,4 @@ class AppUserManager(base_user.BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, is_active, **extra_fields)
